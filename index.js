f3717a180b2a427e7e025158bea854bd7c9bb03a
Alexaner Yefanov <alexander@efanov.net>
2018-10-13 17:43:55 +0200

    Splitting class definitions to multiple files

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-module.exports = require('./lib/product');
-module.exports.FeedBuilder = require('./lib/feedBuilder').FeedBuilder;
+module.exports.Product = require('./lib/product');
+module.exports.ProductPrice = require('./lib/productPrice');
+module.exports.ProductInstallment = require('./lib/productInstallment');
+module.exports.ProductDateRange = require('./lib/productDateRange');
+module.exports.ProductLoyaltyPoints = require('./lib/productLoyaltyPoints');
+module.exports.ProductShipping = require('./lib/productShipping');
+module.exports.ProductTax = require('./lib/productTax');
+module.exports.FeedBuilder = require('./lib/feedBuilder');
+
+Object.assign(module.exports, require('./lib/units'));
